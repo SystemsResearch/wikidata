18d7b45c244c819ce02436ceea7b327d1b1b391d
Dimitri Perrin <dimitri.perrin@qut.edu.au>
2022-09-07 16:17:09 +1000

    Simple unzip file

---
+++ unzip.py
@@ -0,0 +1,33 @@
+import glob, subprocess, datetime
+
+# Input and output folders. Note: make sure you are using the correct folders
+folder_in = "/work/wikidata/"
+folder_out = "/work/wikidata/extracted_files/"
+
+# List of files to process
+
+# Hard-coded files
+files = ["enwiki-20220201-pages-meta-history1.xml-p1p857.7z"]
+
+# Alternatively, we can use `glob` to get a list of all files matching a pattern
+#files = glob.glob("enwiki-20220201-pages-meta-history*.7z")
+
+
+
+# Simple function to print a timestamped message
+def progress_message(txt):
+    # datetime object containing current date and time
+    now = datetime.datetime.now()
+    # time, formatted as dd/mm/YY H:M:S
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    print(dt_string,txt)
+
+
+
+progress_message(f"Starting. Number of files to process: {len(files)}")
+
+for current_file in files:
+        progress_message(f"Processing {current_file}")
+        subprocess.call(["7za","e",folder_in+current_file,"-o"+folder_out])
+
+progress_message("Done.")
